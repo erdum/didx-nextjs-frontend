4f34aad0ccba0915ad7c5a7f76adb901aaa3f4c0
Erdum <erdumadnan@gmail.com>
2023-08-09 17:54:49 +0500

    Update Nunito Sans replaced Raleway

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,9 +16,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;500;600;700;800;900&display=swap" rel="stylesheet"/>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:opsz,wght@6..12,300;6..12,400;6..12,500;6..12,600;6..12,700;6..12,800;6..12,900&family=Raleway:wght@400;500;600;700;800;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Header />
