3ea1267bd015c8a717ddae6c29736dbea5f39d43
Erdum <erdumadnan@gmail.com>
2023-08-09 12:53:14 +0500

    Refactor serve image in friendly format

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ export default function Page() {
     <main className="w-full">
       <article
         className="w-full h-56 bg-top flex items-start justify-center bg-cover bg-no-repeat sm:h-72 lg:h-[36.64vw]"
-        style={{ backgroundImage: "url('/homepage-banner.jpeg')" }}
+        style={{ backgroundImage: "url('/homepage-banner.webp')" }}
       >
         <section className="w-full flex flex-col justify-between items-center gap-y-2 pl-12 pt-12 text-white text-center sm:pl-40 lg:pt-32 lg:gap-y-8 2xl:pt-52 2xl:gap-y-10">
           <h1 className="w-full text-xl font-bold tracking-wider sm:text-3xl lg:text-5xl 2xl:text-7xl">WELCOME TO DIDX</h1>
