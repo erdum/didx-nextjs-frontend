21bf6339ddedabc1d241eb8af53a43a5fb1066b2
Erdum <erdumadnan@gmail.com>
2023-08-10 15:19:18 +0500

    Add talk to an expert cta button added

--- app/page.jsx
+++ app/page.jsx
@@ -13,12 +13,20 @@ export default function Page() {
         <section className="w-full flex flex-col justify-between items-center gap-y-2 pl-12 pt-12 text-white text-center sm:pl-40 lg:pt-32 lg:gap-y-8 2xl:pt-52 2xl:gap-y-10">
           <h1 className="w-full text-xl font-bold tracking-wider sm:text-3xl lg:text-5xl 2xl:text-7xl">WELCOME TO DIDX</h1>
           <h2 className="w-full text-xs font-medium sm:text-lg lg:text-xl 2xl:text-2xl">Connecting the world, one country at a time!</h2>
-          <Link
-            href="/signup"
-            className="self-center mt-6 px-4 py-2 bg-blue-600 text-xl rounded font-semibold transition-all hover:bg-white hover:text-blue-600 hover:scale-110 sm:text-2xl sm:px-6 sm:py-3 sm:mt-8 lg:rounded-lg"
-          >
-            Sign Up
-          </Link>
+          <div className="flex flex-col gap-6 mt-6 xl:flex-row">
+            <Link
+              href="/signup"
+              className="px-4 py-2 bg-blue-600 text-xl rounded font-semibold transition-all hover:bg-zinc-200 hover:text-neutral-600 hover:scale-110 sm:text-2xl sm:px-6 sm:py-3 lg:rounded-lg"
+            >
+              Sign Up
+            </Link>
+            <Link
+              href="/signup"
+              className="hidden px-4 py-2 bg-white text-blue-600 text-xl rounded font-semibold transition-all hover:bg-zinc-200 hover:text-neutral-600 hover:scale-110 sm:text-2xl sm:px-6 sm:py-3 lg:rounded-lg xl:block"
+            >
+              Talk to an Expert
+            </Link>
+          </div>
         </section>
       </section>
 
