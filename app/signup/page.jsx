3e173f81696e71a6de1ef564633a67514f90859e
Erdum <erdumadnan@gmail.com>
2023-08-18 11:50:44 +0500

    Refactor font-size increased

--- app/signup/page.jsx
+++ app/signup/page.jsx
@@ -273,7 +273,7 @@ export default function Page() {
                 <div>
                   <button
                     type="submit"
-                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm font-medium text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
+                    className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-lg font-medium text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-600"
                   >
                     Sign in
                   </button>
