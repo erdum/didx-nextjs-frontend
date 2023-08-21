686d8a16e83989ec31e68ff5b1fc14b8cf0cda7a
Erdum <erdumadnan@gmail.com>
2023-08-22 00:33:44 +0500

    Refactor typo

--- app/coverage/page.jsx
+++ app/coverage/page.jsx
@@ -121,7 +121,7 @@ export default function Page() {
 			<StandardPage>
 				<section className="w-full">
 					<Paragraph>
-						DIDx is the world's No.1 Direct Inward Dialing numbering (DID Numbers) provider since 2005. It's an exchange where \ buyers and sellers meet to trade in DID numbers. We currently have over 100+ countries as part of our coverage. If You need DID Number from a country which you do not see here, Please CONTACT US with your target price and quantity.
+						DIDx is the world's No.1 Direct Inward Dialing numbering (DID Numbers) provider since 2005. It's an exchange where buyers and sellers meet to trade in DID numbers. We currently have over 100+ countries as part of our coverage. If You need DID Number from a country which you do not see here, Please CONTACT US with your target price and quantity.
 					</Paragraph>
 
 					<Paragraph>
