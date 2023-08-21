2864dc6a00daa9d201b9eb4958f1722c7fbed028
Erdum <erdumadnan@gmail.com>
2023-08-21 14:22:35 +0500

    Add theme layout implemented for pages

---
+++ app/layout-components/standardPage.jsx
@@ -0,0 +1,27 @@
+import Link from 'next/link'
+
+export function StandardPage({ children }) {
+	return (
+		<main className="w-full flex flex-col gap-2 px-4 py-8 text-base xl:px-72 xl:flex-row">
+			{ children }
+		</main>
+	)
+}
+
+export function Heading({ children }) {
+	return (
+		<h2 className="uppercase mb-6 mt-10 text-xl text-neutral-600 font-semibold">{ children }</h2>
+	)
+}
+
+export function Paragraph({ children }) {
+	return (
+		<p className="text-neutral-700 my-6">{ children }</p>
+	)
+}
+
+export function Anchor({ children, href = "" }) {
+	return (
+		<Link href={href} className="text-[#2196f3] inline font-semibold">{ children }</Link>
+	)
+}
