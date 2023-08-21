6de2f0e0627046fb28cfc9adfecc751122cf6c3d
Erdum <erdumadnan@gmail.com>
2023-08-21 13:45:49 +0500

    Update main content padding increased

--- app/layout-components/breadCrumb.jsx
+++ app/layout-components/breadCrumb.jsx
@@ -7,7 +7,7 @@ export default function BreadCrumb({ title }) {
 	const path = usePathname().split('/')
 
 	return (
-		<section className="w-full min-h-[64px] bg-zinc-100 flex flex-wrap items-center justify-between px-4 py-2 gap-y-4 xl:px-32">
+		<section className="w-full min-h-[64px] bg-zinc-100 flex flex-wrap items-center justify-between px-4 py-2 gap-y-4 xl:px-72">
 			<p className="text-neutral-600 text-lg">{ title }</p>
 
 			<nav className="text-neutral-500 text-sm font-semibold flex gap-x-1">
