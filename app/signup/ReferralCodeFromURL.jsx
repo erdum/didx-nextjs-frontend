26e622b96241a841e2d35b78cbec75c8794537e4
Erdum <erdumadnan@gmail.com>
2023-08-22 12:30:49 +0500

    Update auto populated referral code can not be mutated

--- app/signup/ReferralCodeFromURL.jsx
+++ app/signup/ReferralCodeFromURL.jsx
@@ -17,6 +17,7 @@ export default function ReferralCodeFromQuery() {
           type="text"
           autoComplete="off"
           defaultValue={referralCode ?? ''}
+          readOnly={referralCode}
           placeholder="Enter referral code and get free $20"
           className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-neutral-500 focus:outline-none focus:ring-blue-600 focus:border-blue-600 sm"
         />
