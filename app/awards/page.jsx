913f50d1b8cb7c2392828c1568586468b4545954
Erdum <erdumadnan@gmail.com>
2023-08-24 17:14:27 +0500

    Update awards page missing links added

--- app/awards/page.jsx
+++ app/awards/page.jsx
@@ -49,7 +49,7 @@ export default function Page() {
               <h3 className="my-4 text-neutral-700 sm:my-0 sm:mb-4 sm:text-lg">
                 DIDX VP OF MARKETING VIA SUPER TECHNOLOGIES WINS 2011 FRIENDS OF KAMAILIO AWARD
               </h3>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://by-miconda.blogspot.com/2012/02/kamailio-2011-awards.html">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -75,7 +75,7 @@ export default function Page() {
               <h3 className="my-4 text-neutral-700 sm:my-0 sm:mb-4 sm:text-lg">
                 DIDX VIA SUPER TECHNOLOGIES WINS THE ARABIA FAST GROWTH 500 AWARD
               </h3>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.allworldlive.com/">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -91,7 +91,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 In 2012, Super Technologies, Inc. maintains an A+ Better Business Bureau rating in the United States of America since 2005.
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.bbb.org/northwestern-florida/business-reviews/internet-services/super-technologies-in-pensacola-fl-13001295">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -107,7 +107,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 IDX empowers thousands of award-winning entrepreneurs, carriers, operators and Internet service providers such as Virtual Phone Line to add revenue never realized before and/or to start new businesses via the power of direct inward dialing. In addition, those that are empowered in this way … in turn … enable their users to have a local presence in areas of the world they are not physically in, so that they are able to receive calls from very important people in those areas to reach new business, social and personal goals.
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.pr.com/press-release/350916">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -123,7 +123,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 Teradata National IT Excellence Awards celebrate accomplishments in the regional IT industry. “Each year we recognize those persons and services who are instrumental in introducing, promoting and implementing IT in Pakistan,” said Khuram Rahat, managing director, Teradata Pakistan. Visit https://www.pr.com/press-release/283337.
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.teradata-awards.com/">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -139,7 +139,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 “The winners of the VON Magazine Innovator Awards should take pride in their accomplishments, and we’d like to thank everyone who submitted a nomination,” said Doug Mohney, Editor-in-Chief of VON Magazine, and Chairman of the awards program. “The scope of the nomination process was extensive, and the quality of submissions was impressive, illustrating a significant level of development and innovation throughout the industry. We look forward to honoring all of the winners at Spring VON.x.”
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.vision2mobile.com/">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -155,7 +155,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 The Innovation Award is designed to recognize developers, customers and partners for outstanding achievements that are improving business processes, overcoming technology challenges and enhancing the company’s bottom line.
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.digium.com/en/company/awards/innovation.php">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -171,7 +171,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 Super Technologies, Inc. has been recognized with a 2006 Product of the Year Award for their excellence in technological advancement and application refinement,” said Rich Tehrani, TMC President and Group Editor-in-Chief of Communications Solutions, “It has proven with its DIDXchange they are committed to quality and excellence in solutions that benefit the customer experience as well as ROI for the companies that use them. I am pleased to honor their hard work and accomplishments and look forward to more innovative solutions from them in the future.
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://news.tmcnet.com/news/2007/04/30/2564473.htm">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -187,7 +187,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 This annual Awards program also provides an opportunity and an ideal environment for companies to gain local, regional and international exposure through on-going promotional activities, and through participation at the international Asia Pacific ICT Awards (APICTA). The top winners of the P@SHA ICT Awards (only product categories) will be nominated for representation at the annual Asia Pacific Awards.
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://pasha.org.pk/html_files/newsroom_news67.html">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -203,7 +203,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 Pensacola, FL. Sept 18, 2007 Super Technologies, Inc. announced today that Technology Marketing Corporation has namedDIDXchange as a recipient of a 2007 INTERNET TELEPHONY Excellence Award presented by INTERNET TELEPHONYmagazine (www.itmag .com)
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.tmcnet.com/usubmit/2007/09/19/2951250.htm">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -219,7 +219,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 INTERNET TELEPHONY editors selected the companies which in their view demonstrate the vision, leadership and attention to detail that are the hallmarks of the prestigious Product of the Year award winners who include DIDXchange, Avaya One-X Deskphone for 9600 Series IP Telephones, Citrix Application Gateway™, eyeBeam 1.5, Truphone and others. LAUNCH SITE
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.tmcnet.com/usubmit/2007/01/03/2213308.htm">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -235,7 +235,6 @@ export default function Page() {
               <p className="text-neutral-600">
                 The editorial staff of Internet Telephony magazine is proud to announce the winners of this year’s Internet Telephony Excellence Awards. These companies are as varied as there are products that fit under the IP telephony umbrella. In fact the only thing more varied than the vendors serving their customers are the customers themselves. And, as we can see from the following companies, they are increasingly turning to IP Communications solutions to build their businesses, and grow their bottom lines.
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -251,7 +250,6 @@ export default function Page() {
               <p className="text-neutral-600">
                 Pensacola, FL, August 17, 2006Pensacola, FL, August 17, 2006. Total Telecom’s panel of judges selected Super Technologies Inc.’s product DIDXchange to be on its Communication Changermaker Shortlist. Awards will be showcased at London Marriott Hotel on
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
@@ -267,7 +265,7 @@ export default function Page() {
               <p className="text-neutral-600">
                 The Asia Pacific ICT Awards (APICTA) is an international Awards Program initiated by the Multimedia Development Corporation of Malaysia to increase ICT awareness in the community and assist in bridging the Digital Divide. By providing networking and product benchmarking opportunities to ICT innovators and entrepreneurs in the region, the program is designed to stimulate ICT innovation and creativity, promote economic and trade relations, facilitate technology transfer, and offer business matching opportunities via exposure to venture capitalists and investors.
               </p>
-              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://getvoip.com/blog/2015/04/22/top-50-uc-experts">View more</Link>
+              <Link className="bg-blue-600 text-white text-lg self-start px-3 py-1.5 mt-4 rounded" href="https://www.apicta.com/">View more</Link>
             </div>
           </div>
           <div className="w-full h-px bg-neutral-300 my-12 flex items-center justify-center xl:mt-4">
