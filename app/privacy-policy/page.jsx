cab1f76a77d53203f691dac4eab37f59e6475e16
Erdum <erdumadnan@gmail.com>
2023-08-28 16:46:51 +0500

    Add privacy policy page implemented

---
+++ app/privacy-policy/page.jsx
@@ -0,0 +1,133 @@
+import BreadCrumb from '../layout-components/breadCrumb'
+import { StandardPage, Heading, SubHeading, Paragraph, Anchor } from '../layout-components/standardPage'
+
+export const metadata = {
+	'title': 'Privacy Policy'
+}
+
+export default function Page() {
+	return (
+		<>
+			<BreadCrumb title={metadata.title} />
+			<StandardPage>
+				<section className="w-full">
+					<Heading>
+						Privacy Policy
+					</Heading>
+					<Paragraph>
+						The policies below are applicable to all of Super Technologies, Inc.’s websites (parent site www.supertec .com.), further listed as Supertec.
+					</Paragraph>
+					<Paragraph>
+						1. <Anchor href="#general">General Information</Anchor><br />
+						2. <Anchor href="#security">Security Measures</Anchor><br />
+						3. <Anchor href="#pi-info">Persoanlly Identifiable Information</Anchor><br />
+						4. <Anchor href="#non-pi-info">Non-Persoanlly Identifiable Information Collected Automatically</Anchor><br />
+						5. <Anchor href="#use-of-cookies">Use of Cookies</Anchor><br />
+						6. <Anchor href="#gdpr">GDPR</Anchor><br />
+						7. <Anchor href="#declining-email-offers">Declining Email Offers</Anchor><br />
+						8. <Anchor href="#who-to-contact">Who to Contact</Anchor>
+					</Paragraph>
+
+					<SubHeading id="general">
+						1. General
+					</SubHeading>
+					<Paragraph>
+						Supertec respects the privacy of each individual who visits its web sites, who responds to our interactive advertisements and who sends us e-mail. This Privacy Policy outlines the information Supertec will collect and how we will use that information. This Policy will instruct you on what to do if you do not want your personal information collected or shared when you visit Supertec web sites, when you respond to our Web-based advertisements or and when you send us email.
+					</Paragraph>
+
+					<SubHeading id="security">
+						2. Security
+					</SubHeading>
+					<Paragraph>
+						All online order forms from Supertec make available a Secure Socket Layer (SSL) protocol to provide “secure sessions” for our visitors with “s” added after http. Through the “secure session,” information that you input into a Supertec online order form will be sent and will arrive privately and unaltered at a Supertec server. This security prohibits other companies and web users from being able to access your information.
+					</Paragraph>
+
+					<SubHeading id="pi-info">
+						3. Personal-Identifiable Information
+					</SubHeading>
+					<Paragraph>
+						Supertec will not collect any personally-identifiable information about you (your name, address, telephone number, or email address) unless you provide it to us voluntarily.
+					</Paragraph>
+					<Paragraph>
+						The following are a few examples of when Supertec may require that you provide personally-identifiable information:
+					</Paragraph>
+					<ul className="list-disc pl-8 mb-6">
+						<li>to complete an online order for a product or service</li>
+						<li>to signup for our newsletter or other advertisements</li>
+						<li>to request more information about new or current Supertec products or services</li>
+					</ul>
+					<Paragraph>
+						If you do not want your personally-identifiable information collected, do not submit it to us. If you have already submitted this information and would like for us to remove it from our records, please contact us at the email address listed at the bottom of this Statement. We will use reasonable efforts to delete your information from our existing files.
+					</Paragraph>
+					<Paragraph>
+						When you do provide us with personally-identifiable information, we may use that information in the following ways, unless stated otherwise:
+					</Paragraph>
+					<ul className="list-disc pl-8 mb-6">
+						<li>we may store and process that information to better understand all customer needs and how we can improve our products and services for all customers</li>
+						<li>we may use that information to contact you with new promotions and announcements if you have previously provided your permission</li>
+						<li>we may contact you if we have questions about your account</li>
+					</ul>
+					<Paragraph>
+						Supertec will not sell personally-identifiable information to third parties, unless stated at the time of collection. We are committed to giving you the choice whether or not we use your information for marketing purposes or share information with business partners.
+					</Paragraph>
+
+					<SubHeading id="non-pi-info">
+						4. Non-Persoanlly Identifiable Information
+					</SubHeading>
+					<Paragraph>
+						In some cases, we may collect information about you that is not personally-identifiable. Examples of this type of information include the type of Internet Browser you are using, the type of computer operating system you are using, and the domain name of the Website and/or Internet Service Provider from which you linked to our site or advertisement.
+					</Paragraph>
+					<Paragraph>
+						Similar to personally identifiable information, we will not sell non-personally-identifiable information to third parties, unless stated at the time of collection. We may share non-personally identifiable information with business partners.
+					</Paragraph>
+
+					<SubHeading id="use-of-cookies">
+						5. Use of Cookies
+					</SubHeading>
+					<Paragraph>
+						When you view one of our web sites or advertisements, we may store some information on your computer. This information will be in the form of a “Cookie” or similar file and will be used to determine ways to improve our web sites, advertisements, products or services. For example, cookies allow us to tailor a web site to better match your interests and preferences. With most Internet Browsers, you can erase Cookies from your computer hard drive, block all Cookies, or receive a warning before a Cookie is stored. Please refer to your browser instructions or help screen to learn more about these functions.
+					</Paragraph>
+
+					<SubHeading id="gdpr">
+						6. GDPR
+					</SubHeading>
+					<Paragraph>
+						"Our industry participants count on DIDx to be worthy of trust in the cloud. We take GDPR as the chance to prove we are worthy of that trust and to set the bar for data privacy and protection as very serious and a must-have." CEO, Rehan Allahwala
+					</Paragraph>
+					<Paragraph>
+						DIDx complies with all data privacy laws and directives, including the PwC of USA and the General Data Protection Regulation (EU) 2016/679 (“GDPR”).
+					</Paragraph>
+					<Paragraph>
+						DIDx enables its clients and vendors to interface their products in our and each others' software applications. We do not have a direct relationship with any of their end users. When clients share end user data, we approach the data with a goal to comply with data protection regulations, including the GDPR and PwC. We refuse to and do not use any of that data for purposes except those that are expressly required by the customer who gives us the data. The other reason to use the data is onnly as is required under law.
+					</Paragraph>
+					<Paragraph>
+						Our compliance with the GDPR and PwC policies, we want to refer to the terms “controller” and “processor." A “controller” is the business that an individual (“data subject”) shares personal data with. The "controller" decides the reason for the use of the personal data and is accountable for the law-abiding use of the data subject’s data. A “processor” is the business that makes available any of part services to the controller and requires particular personal data under the control of the controller.
+					</Paragraph>
+					<Paragraph>
+						DIDx works with vendors and buyers. It is a medium for enabling the purchase and sales of DID numbers among vendors and buyers. During different aspects of business, DIDx may be a controller and at other times a processor. When DIDx processes the communications media and data provided by a client, such as between DIDx and the customer, typically DIDx is the processor. When DIDx processes communications metadata, DIDx is a processor. It may be deemed necessary to work as a controller in others. When DIDx processes a client or vendor's company or personal data that either has provided through our online marketplace or other, DIDx acts the part of a controller. As DIDx acts as controller or processor, it does everything possible to make sure that the entities we collaborate with will adhere to GDPR and PwC. We do everything in our power to make sure that any other party will not manipulate, manage or use the data in any way that that our client or vendor has relied upon us for any reason than providing services purchased.
+					</Paragraph>
+					<SubHeading>
+						Object to and restrict the processing of data
+					</SubHeading>
+					<Paragraph>
+						In any case that a client or vendor's company or personal data appears to be processed to fulfill a common interest to DIDx, like PR or marketing, the company or individual has every right to object to and unsubscribe to DIDx. We welcome these objections and requests to unsubscribe to made to legal@didx.net. Per GDPR and PwC, each client and vendor has every right to restrict processing. DIDx will ensure to process client or vendor data each requests. DIDx will take each request seriously and as a one-on-one basis out of business respect and according to the rules set out by the GDPR and PwC. In any case where DIDx cannot comply with the request, we will demonstrate our legal and compelling grounds to do that. In some cases, there are legal claims that enable DIDx to retain personal data. In any case in which the client or vendor does not agree with the manner that we handle any request, the client or vendor has a right to file a complaint with the government authorities related to the member state or country that the client or vendor resides or works in, as well in any state or country where suspected infringement occurs
+					</Paragraph>
+
+					<SubHeading id="declining-email-offers">
+						7. Declining Email Offers
+					</SubHeading>
+					<Paragraph>
+						As outlined in Section III. of this Policy, we will only send email promotions or announcements to you if you have previously provided your permission for us to do so. Although many customers tell us they appreciate receiving notice of these carefully designed promotions and announcements, we recognize the importance of providing you with choices. At any time, you may unsubscribe from our e-mail list by filling out a simple online un-subscribe form. All e-mail offers that you receive from Supertec will inform you of how to decline receiving further email offers.
+					</Paragraph>
+
+					<SubHeading id="who-to-contact">
+						8. Who to Contact
+					</SubHeading>
+					<Paragraph>
+						If you have submitted personally-identifiable information through a Supertec Web site or interactive advertisement, and would like that information deleted from our records, please contact us at our email address, care(at)supertec.com. We will use reasonable efforts to delete this information from our existing files.`
+					</Paragraph>
+				</section>
+			</StandardPage>
+		</>
+	)
+}
