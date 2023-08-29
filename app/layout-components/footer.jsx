54bce97f639f3ed2da6f3fa95a3e76d6827245cf
Erdum <erdumadnan@gmail.com>
2023-08-29 11:24:25 +0500

    Update footer styling, icon, colors reverted to match old site styling as per requested

--- app/layout-components/footer.jsx
+++ app/layout-components/footer.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faArrowRight, faPhone, faMessage, faRss, faEnvelope } from "@fortawesome/free-solid-svg-icons"
+import { faHandPointRight, faPhone, faMessage, faRss, faEnvelope } from "@fortawesome/free-solid-svg-icons"
 import {
 	faYoutube,
 	faTwitter,
@@ -15,9 +15,9 @@ import isoCertificateImg from "../../public/iso-certificate.png"
 
 export default function Footer() {
 	return(
-		<footer className="w-full flex flex-col bg-neutral-700">
-			<div className="flex flex-col px-4 py-8 lg:flex-row lg:gap-x-10 lg:px-28 xl:px-32">
-				<div className="w-full flex flex-col gap-y-3 text-neutral-300 font-`">
+		<footer className="w-full flex flex-col bg-[#333333]">
+			<div className="flex flex-col px-4 py-8 lg:flex-row lg:gap-x-10 lg:px-28 xl:px-80">
+				<div className="w-full flex flex-col gap-y-3 text-[#ffffff80] font-`">
 					<Image
 						src={logo}
 						alt="DIDX Inc logo"
@@ -28,33 +28,33 @@ export default function Footer() {
 					<p><span className="text-sm font-medium">Email:</span><br />sales@didx.net</p>
 				</div>
 
-				<nav className="w-full flex flex-col items-start gap-y-3 text-neutral-300 font-medium">
+				<nav className="w-full flex flex-col items-start gap-y-3 text-[#ffffff80] font-medium">
 					<p className="text-white text-2xl mt-6 mb-4">DIDx Seller & Buyer</p>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/clec"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>CLECs Sell Numbers</Link>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/toll-free-numbers"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>Toll Free Numbers</Link>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/case-study"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>Case Study</Link>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/testimonial"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>Testimonial</Link>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/coverage"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>Coverage Area</Link>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/lnp-lookup"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>LNP Lookup</Link>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/our-team"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>Our Team</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/clec"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>CLECs Sell Numbers</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/toll-free-numbers"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>Toll Free Numbers</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/case-study"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>Case Study</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/testimonial"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>Testimonial</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/coverage"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>Coverage Area</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/lnp-lookup"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>LNP Lookup</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/our-team"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>Our Team</Link>
 					<span className="w-4/5 h-px mr-auto mt-6 bg-neutral-400 sm:w-2/5 xl:w-full"></span>
 					<Link href="https://youtu.be/FOuPb9qgFlI" className="w-full flex flex-wrap">
 						<FontAwesomeIcon className="text-4xl bg-[#ff8c00] text-white p-2 rounded-md transition-colors hover:text-[#ff8c00] hover:bg-white" icon={faYoutube}/>
-						<span className="mt-1 ml-3 text-neutral-300">Training Videos<br /> Click Here</span>
+						<span className="mt-1 ml-3 text-[#ffffff80]">Training Videos<br /> Click Here</span>
 					</Link>
 				</nav>
 
-				<nav className="w-full flex flex-col items-start gap-y-3 text-neutral-300 font-medium">
+				<nav className="w-full flex flex-col items-start gap-y-3 text-[#ffffff80] font-medium">
 					<p className="text-white text-2xl mt-6 mb-4">Sample Configuration</p>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/asterisk"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>Asterisk</Link>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/voip-switch"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>VOIP Switch</Link>
-					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/elastix"><FontAwesomeIcon icon={faArrowRight} className="mr-2"/>Elastix</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/asterisk"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>Asterisk</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/voip-switch"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>VOIP Switch</Link>
+					<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/elastix"><FontAwesomeIcon icon={faHandPointRight} className="mr-2"/>Elastix</Link>
 					<span className="w-4/5 h-px mr-auto mt-6 bg-neutral-400 sm:w-2/5 xl:w-full"></span>
 					<Image src={businessCertificateImg} alt="accredited business certificate" />
 					<Image src={isoCertificateImg} alt="ISO 9001:2008 registeration mark" />
 				</nav>
 
-				<nav className="w-full flex flex-col items-start gap-y-3 text-neutral-300 font-medium">
+				<nav className="w-full flex flex-col items-start gap-y-3 text-[#ffffff80] font-medium">
 					<p className="text-white text-2xl mt-6 mb-4">Subscribe To Our Newsletter</p>
 					<input type="text" name="email" placeholder="Email*" className="py-1 px-2 rounded my-2 border border-white bg-neutral-800 outline-none placeholder:text-white"/><br />
 					<button className="text-white bg-blue-600 px-3 py-1.5 rounded transition-colors hover:text-blue-600 hover:bg-white">Join List</button>
@@ -62,21 +62,21 @@ export default function Footer() {
 					<div className="w-full flex justify-between">
 						<Link href="calto:salesdidx" className="w-full flex justify-start">
 							<FontAwesomeIcon className="text-2xl bg-[#ff8c00] text-white p-2 rounded-md transition-colors hover:text-[#ff8c00] hover:bg-white" icon={faPhone}/>
-							<span className="ml-3 text-neutral-300 text-sm">Call us<br /> on Skype</span>
+							<span className="ml-3 text-[#ffffff80] text-sm">Call us<br /> on Skype</span>
 						</Link>
 						<Link href="calto:salesdidx?chat" className="w-full flex justify-start">
 							<FontAwesomeIcon className="text-2xl bg-[#ff8c00] text-white p-2 rounded-md transition-colors hover:text-[#ff8c00] hover:bg-white" icon={faMessage}/>
-							<span className="ml-3 text-neutral-300 text-sm">Chat with us<br /> on Skype</span>
+							<span className="ml-3 text-[#ffffff80] text-sm">Chat with us<br /> on Skype</span>
 						</Link>
 					</div>
 				</nav>
 			</div>
 
-			<div className="w-full min-h-20 flex flex-wrap gap-2 justify-between px-4 py-6 bg-neutral-800 text-neutral-300 lg:px-28 xl:px-30">
+			<div className="w-full min-h-20 flex flex-wrap gap-2 justify-between px-4 py-6 bg-[#292929] text-[#ffffff80] lg:px-28 xl:px-80">
 				<p>
 					DIDX is Link Delaware based company since 2005. &#169; {new Date().toLocaleString('en-us', { year: "numeric" })} DIDX, Inc.
 					<br />
-					<span className="text-neutral-300 text-sm">
+					<span className="text-[#ffffff80] text-sm">
 						<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/terms">Terms of Services</Link> |
 						<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/privacy-policy"> Privacy Policy</Link> |
 						<Link className="transition-colors hover:text-neutral-500 hover:cursor-pointer" href="/use-policy"> Use Policy</Link> |
