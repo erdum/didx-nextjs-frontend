a3e8c251f20694ca7e19f7cafcb3c065370cc9b8
Erdum <erdumadnan@gmail.com>
2023-08-29 10:54:56 +0500

    Add use policy page implemented

---
+++ app/use-policy/page.jsx
@@ -0,0 +1,160 @@
+import BreadCrumb from '../layout-components/breadCrumb'
+import { StandardPage, Heading, SubHeading, SmallHeading, Paragraph, Anchor } from '../layout-components/standardPage'
+
+export const metadata = {
+	'title': 'Use Policy | DIDX'
+}
+
+export default function Page() {
+	return (
+		<>
+			<BreadCrumb title={metadata.title} />
+			<StandardPage>
+				<section className="w-full px-16">
+					<h3 className="bg-[#0D6EFD] text-white text-center text-2xl">#Acceptable Use Policy</h3>
+					<h4 className="my-6">SUPER TECHNOLOGIES, INC. ACCEPTABLE USE POLICY <span>Updated: January 1, 2008</span></h4>
+					<p>
+						Super Technologies, Inc. (“STec”) has created this Acceptable Use Policy (the “Policy”) so you will understand when and under what circumstances we may suspend or terminate your use of your services (the "Service") and access to Super Technologies’ service related Internet sites, which sites contain information and data available via your computer and/or a device (the "STec Sites"). By using our Service or accessing the STec Sites, you consent to the acceptable use practices described in this Policy, as modified from time-to-time by us.
+					</p>
+					<SmallHeading>
+						Content Prohibitions
+					</SmallHeading>
+					<Paragraph>
+						Our goal is for our subscribers to have a pleasurable experience each time they access the STec Sites or use our Service. Therefore, you may only use our Service for lawful purposes and in a manner that does not interfere with other subscribers or our systems. To assure this, we reserve the right, but are not obligated, to suspend or terminate your access to the STec Sites and/or your use of the Service at any time, if we determine in our sole discretion that your conduct or behavior on the STec Sites or our Service involves or causes:
+					</Paragraph>
+
+					<Paragraph>
+						1. Content Harmful or Offensive to Third-Parties:
+					</Paragraph>
+					<Paragraph>
+						Do not upload, download, post, distribute, publish, or otherwise transmit (collectively, "Disclose") any message, data, information, image, text, termination minutes, DID or other material (collectively, "Content") that is unlawful, libelous, defamatory, slanderous, obscene, pornographic, indecent, lewd, harassing, threatening, harmful, invasive of privacy or publicity rights, abusive, inflammatory, or otherwise harmful or offensive to third parties;
+					</Paragraph>
+
+					<Paragraph>
+						2. Content Harmful to Other Systems:
+					</Paragraph>
+					<Paragraph>
+						Do not Disclose harmful Content, including without limitation, viruses, Trojan horses, worms, time bombs, zombies, cancelbots, or any other computer programming routines that may damage, interfere with, surreptitiously intercept or expropriate any system, program, data, or personal information, via our Service;
+					</Paragraph>
+
+					<Paragraph>
+						3. Impersonation:
+					</Paragraph>
+					<Paragraph>
+						Do not impersonate any person or entity or otherwise misrepresent your affiliation with a person or entity;
+					</Paragraph>
+
+					<Paragraph>
+						4. Infringing Content:
+					</Paragraph>
+					<Paragraph>
+						Do not Disclose any Content that may infringe any patent, trademark, trade secret, copyright or other intellectual or proprietary right of any party. Infringement may result from the unauthorized copying and posting or distributing graphics, DID, termination minutes, pictures, photographs, logos, software, articles, music, or videos. By posting any Content, you represent and warrant that you have the lawful right to distribute and reproduce such Content;
+					</Paragraph>
+
+					<Paragraph>
+						5. Interference:
+					</Paragraph>
+					<Paragraph>
+						Do not do anything to interfere with other users of Services;
+					</Paragraph>
+
+					<Paragraph>
+						5. Unapproved Promotions or Advertising of Goods or Services:
+					</Paragraph>
+					<Paragraph>
+						Without our prior written permission, do not Disclose on the Sites any unsolicited promotions of goods or services, commercial solicitation or any advertising, promotional materials, or any other solicitation of other users for goods or services except in those areas (e.g., Tiki support sites) that are designated for such purpose;
+					</Paragraph>
+
+					<Paragraph>
+						5. Unlawful Content:
+					</Paragraph>
+					<Paragraph>
+						Do not Disclose any Content that would constitute or encourage a criminal offense, violate the rights of any party, or that would otherwise create liability or violate any local, state, national, or international law.
+					</Paragraph>
+
+
+					<SmallHeading>
+						Network Usage
+					</SmallHeading>
+					<Paragraph>
+						Violations of system or network security are prohibited, and may result in criminal and civil liability. STec may investigate incidents involving such violations and may involve and will cooperate with law enforcement if a criminal violation is suspected. Examples of system or network security violations include, without limitation, the following:
+					</Paragraph>
+
+					<Paragraph>
+						1. Avoiding System Restrictions:
+					</Paragraph>
+					<Paragraph>
+						Using manual or electronic means to avoid any use limitations placed on the Services.
+					</Paragraph>
+
+					<Paragraph>
+						2. Falsification of Origin:
+					</Paragraph>
+					<Paragraph>
+						Forging any TCP-IP packet header, e-mail header, or any part of a message header. This prohibition does not include the use of aliases or anonymous remailers.
+					</Paragraph>
+
+					<Paragraph>
+						3. Hacking:
+					</Paragraph>
+					<Paragraph>
+						Unauthorized access to or use of data, systems, or networks, including any attempt to probe, scan, or test the vulnerability of a system or network or to breach security or authentication measures without express authorization of the owner of the system or network.
+					</Paragraph>
+
+					<Paragraph>
+						4. Interception:
+					</Paragraph>
+					<Paragraph>
+						Unauthorized monitoring of data or traffic on any network or system without express authorization of the owner of the system or network.
+					</Paragraph>
+
+					<Paragraph>
+						5. International Interference:
+					</Paragraph>
+					<Paragraph>
+						Interference with service to any user, host, or network including, without limitation, mail bombing, news bombing, other flooding techniques, deliberate attempts to overload a system, or broadcast attacks.
+					</Paragraph>
+
+					<Paragraph>
+						6. Modifications to Transmission:
+					</Paragraph>
+					<Paragraph>
+						Installation of any amplifiers, enhancers, repeaters, or other devices that modify, disrupt, or interfere in any way with the radio frequency licensed to us to provide Service.
+					</Paragraph>
+
+					<SmallHeading>
+						Our Responsibility
+					</SmallHeading>
+					<Paragraph>
+						We take no responsibility and assume no liability for any Content uploaded, transmitted, or downloaded by you or any third party, or for any mistakes, defamation, slander, libel, omissions, falsehoods, obscenity, pornography, or profanity you may encounter. As the provider of the STec Sites and Service, we are only a marketplace and are not liable for any statements, representations, or Content provided by our users in any public forum. It is not our objective to discourage you from taking controversial positions or expressing vigorously what may be unpopular views. However, we reserve the right to take such action as we deem appropriate in cases where the STec Sites or Service is used to disseminate statements which are offensive or harmful.
+					</Paragraph>
+
+					<SmallHeading>
+						Enforcment of This Policy
+					</SmallHeading>
+					<Paragraph>
+						We reserve the right, but do not assume the obligation, to strictly enforce this Policy by, without limitation, issuing warnings, suspending, or terminating Service, refusing to transmit, removing, screening, or editing Content prior to delivery or actively investigating violations and prosecuting them in any court or appropriate venue. We may block access to certain categories of numbers (e.g. certain international destinations) or certain sites if, in our sole discretion, we are experiencing excessive billing, collection, fraud problems or other misuse of our network.
+					</Paragraph>
+					<Paragraph>
+						We may access, use, and disclose transaction information about your use of our Service, and any Content transmitted by you via the STec Sites or through the Service, to the extent permitted by law, in order to comply with the law (e.g., a lawful subpoena); to enforce or apply our subscriber agreements; to initiate, render, bill, and collect for our Services; to protect our rights or property, or to protect users of our Services from fraudulent, abusive, or unlawful use of, or subscription to, our Service. INDIRECT OR ATTEMPTED VIOLATIONS OF THIS POLICY OR ANY RELATED POLICY, GUIDELINE OR AGREEMENT, AND ACTUAL OR ATTEMPTED VIOLATIONS BY A THIRD PARTY ON YOUR BEHALF SHALL BE CONSIDERED VIOLATIONS OF THIS POLICY BY YOU.
+					</Paragraph>
+
+					<SmallHeading>
+						Your Responsibility
+					</SmallHeading>
+					<Paragraph>
+						You are solely liable for any transmissions you initiate through our Service or any Content you Disclose. Unless you are participating in an area of the STec Sites or Service that requires or allows anonymity, you will always use your real name in online communications. You agree to indemnify and hold us harmless from any claim, action, demand, loss, or damage (including attorneys’ fees) made or incurred by any third party arising out of or relating to your violation of this Policy.
+					</Paragraph>
+
+					<SmallHeading>
+						Updating this Policy
+					</SmallHeading>
+					<Paragraph>
+						We will revise and update this Policy if our practices change, as technology changes, or as we add new services or change existing ones. If we make any material changes to this Policy, we will post an "update" notice to the STec Sites and change the date of the Policy. You should refer back to this page for the latest information and to be privy to your acceptable use obligations.
+					</Paragraph>
+					<Paragraph><Anchor href="/contact-us">Contact us for more information</Anchor>.</Paragraph>
+				</section>
+			</StandardPage>
+		</>
+	)
+}
