e51a80340eb35f00f32661bbf702ea789a2fb08c
Erdum <erdumadnan@gmail.com>
2023-08-30 11:06:51 +0500

    Add mobile page implemented

---
+++ app/mobile/page.jsx
@@ -0,0 +1,165 @@
+import BreadCrumb from '../layout-components/breadCrumb'
+import { StandardPage, Heading, SmallHeading, Paragraph } from '../layout-components/standardPage'
+
+export const metadata = {
+	'title': 'How To Make Money From DIDX | DIDX'
+}
+
+export default function Page() {
+	return (
+		<>
+			<BreadCrumb title={metadata.title} />
+			<StandardPage>
+				<section className="w-full xl:px-16">
+					<Heading>
+						How to make money from DIDX
+					</Heading>
+
+					<SmallHeading>
+						For You, Mobile Phone Operator: (Outside North America)
+					</SmallHeading>
+					
+					<Paragraph>
+						Mobile operators require as many subscribers as possible, with the least possible cost. In fact, the ARPU is based on the incoming calls and outgoing calls even though more profit is made from incoming calls.
+					</Paragraph>
+
+					<Paragraph>
+						The acquisition cost of each customer increases as competition become more intense.
+					</Paragraph>
+
+					<Paragraph>
+						Other costs, such as customer service, marketing, BTU installation, tower installation, tower location, and maintenance add to the mobile phone operator’s worries.
+					</Paragraph>
+
+					<Paragraph>
+						What if there is a way to increase the number of users, increase the incoming call volume, and have ZERO customer acquisition cost, almost no maintenance cost, no BTU cost, and no customer service cost?
+					</Paragraph>
+
+					<Paragraph>
+						It is possible with the mobile phone operator’s participation in DIDX.
+					</Paragraph>
+
+					<SmallHeading>
+						Currently, your customers are located where its towers and coverage are, and most often in one licensed country, but what if you had millions of users outside your licensed country on roaming, and you could make additional money on the incoming calls?
+					</SmallHeading>
+
+					<Paragraph>
+						Be a global telecom company with customers all over the world – in the United States, the Middle East, China, India, and other parts of Asia.
+					</Paragraph>
+
+					<Paragraph>
+						Suddenly, someone else has all the marketing cost, but you keep the income on the calls.
+					</Paragraph>
+
+					<Paragraph>
+						Suddenly, your market expands and you are global.
+					</Paragraph>
+
+					<Paragraph>
+						DIDX empowers you to offer your mobile numbers to the world, and make revenue from a market that was never before possible.
+					</Paragraph>
+
+					<Paragraph>
+						DIDX empowers you to offer your mobile numbers to the world, and make revenue from a market that was never before possible.
+					</Paragraph>
+
+					<Paragraph>
+						Your DID numbers will ring on end-users’ Yahoo Messengers in Russia or North America or even China.
+					</Paragraph>
+
+					<Paragraph>
+						The NGN operators have an estimated 700 Million + subscribers, a HUGE market now yours.
+					</Paragraph>
+
+					<Paragraph>
+						Join DIDXchange. Make your DID available to over 3000 next-gen telecom companies around the world, who are ready to buy from you via DIDX and then sell your numbers to their customers.
+					</Paragraph>
+
+					<Paragraph>
+						Start sharing the incoming revenue now. Your sales will multiply and bring new services to life.
+					</Paragraph>
+
+					<Paragraph>
+						DIDXchange predicts within 2 years’ time, you should have 3 times more subscribers than you have now. Your cost for these new subscribers is ZERO because someone else is actually selling all these numbers for you and providing all the customer service for you.
+					</Paragraph>
+
+					<Paragraph>
+						Give it a try. Point 1000 numbers of yours to DIDX.net over IP protocol. Your NGN is probably enabled. If not, we can provide you TDM to IP conversion boxes.
+					</Paragraph>
+
+					<Paragraph>
+						DIDX does: All the billing All the collection All the customer service All the guarantee of the payments
+					</Paragraph>
+
+					<Paragraph>
+						You get: A monthly income on the rental of the phone number
+					</Paragraph>
+
+					<Paragraph>
+						Incoming call interconnect fee from the local user and other mobile or land line operator
+					</Paragraph>
+
+					<Paragraph>
+						Guaranteed payments Complete end to end support with dedicated account managers Take advantage of being the licensed operator in your market and earn revenue from countries that you have never imagined before.Give it a try, you have nothing to loose and a world to gain. Let one of our consultants call you and advise you on how to proceed.
+					</Paragraph>
+
+					<SmallHeading>
+						Increase Your ARPU today !!
+					</SmallHeading>
+
+
+					<SmallHeading>
+						For Mobile Operators in North America, Hong Kong, CDMA operators:
+					</SmallHeading>
+
+					<Paragraph>
+						Today, you provide your customers with a local phone number, and you do not charge for incoming call. How about increasing that ARPU?
+					</Paragraph>
+
+					<Paragraph>
+						DIDX empowers you to offer your customer the option to buy numbers from other area codes than the ones you currently provider. They pay you an extra monthly service fee for this and you increase your ARPU by 30% or more.
+					</Paragraph>
+
+					<SmallHeading>
+						Scenario 1:
+					</SmallHeading>
+					<Paragraph>
+						Customer is in New York. His mom Lives in Tampa. Currently Mom has to call a long distance number to reach the person in New York, but for an extra $10, the customer can get a Tampa phone number, that will ring on the customer’s cell phone in Seattle. The mom is now virtually and psychologically (which is good) more inclined to call the customer by dialing the phone number that is local to her. She feels more attached to the customer, and finds him nearer which is important to her.
+					</Paragraph>
+
+					<SmallHeading>
+						Technically how it will work:
+					</SmallHeading>
+					<Paragraph>
+						The call will generate on our DIDX network in Tampa, travel over IP to your network in New York, deliver the call to your switch in Seattle or nearest interconnect, and finally deliver the call to the customer locally, thus removing the long distance leg.
+					</Paragraph>
+
+					<SmallHeading>
+						Scenario 2:
+					</SmallHeading>
+					<Paragraph>
+						Customer lives in Seattle. Customer’s girlfriend lives in London. Customer pays $10 to $15 more to get a London phone number on his cell phone in Seattle.
+					</Paragraph>
+					<Paragraph>
+						The customer’s girlfriend feels closer to the customer. The customer is willing to pay $15 for this service. Customer’s girlfriend dials local and reaches the customer in Seattle.
+					</Paragraph>
+
+					<SmallHeading>
+						Technically how it will work:
+					</SmallHeading>
+					<Paragraph>
+						The call will generate on DIDX network partner’s location in London, travel over IP to your network in Seattle, deliver the call to your switch in Seattle or nearest interconnect, and deliver the call to the customer locally. This remove the long distance leg and generates a new revenue stream thus increasing your ARPU.
+					</Paragraph>
+
+					<Paragraph>
+						With DIDX, your customer selects the number from any part of the world, pays you, and instantly we route that number to your pre-defined switch and the number is provisioned.
+					</Paragraph>
+
+					<Paragraph>
+						You decide the price and billing plan to charge your customer for this service. We charge you a wholesale fee only and pay to our partner network that you choose the number from.
+					</Paragraph>
+				</section>
+			</StandardPage>
+		</>
+	)
+}
