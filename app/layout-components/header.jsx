435f7628576c6dcde2ad53e2853da9f3210087e9
Erdum <erdumadnan@gmail.com>
2023-10-03 10:33:25 +0500

    Fix header transparency issue

--- app/layout-components/header.jsx
+++ app/layout-components/header.jsx
@@ -31,7 +31,7 @@ export default function Header() {
 	}, [isMenuOpen])
 
 	return (
-		<header className="w-full h-20 px-3 flex items-center flex-wrap bg-[#ffffffcc] sticky top-0 z-50 shadow-md lg:h-24 lg:px-8">
+		<header className="w-full h-20 px-3 flex items-center flex-wrap bg-white lg:bg-[#ffffffcc] sticky top-0 z-50 shadow-md lg:h-24 lg:px-8">
 			<Link href="/" className="mr-auto hover:cursor-pointer">
 				<img src="/didx-logo.png" className="w-[100px] h-full lg:w-[120px]" />
 			</Link>
@@ -43,7 +43,7 @@ export default function Header() {
 				<span className="block w-6 h-0.5 bg-gray-500"></span>
 				<span className="block w-6 h-0.5 bg-gray-500"></span>
 			</button>
-			<nav id="mobile-menu" className="w-full h-0 absolute top-20 left-0 flex flex-col items-start justify-start gap-y-6 overflow-hidden font-semibold px-5 text-[#444444] bg-white z-10 transition-all duration-300 ease-in uppercase lg:px-8 xl:px-0 xl:relative xl:top-0 xl:w-auto xl:h-auto xl:gap-x-6 xl:flex-row">
+			<nav id="mobile-menu" className="w-full h-0 absolute top-20 left-0 flex flex-col items-start justify-start gap-y-6 overflow-hidden font-semibold px-5 text-[#444444] bg-white lg:bg-transparent z-10 transition-all duration-300 ease-in uppercase lg:px-8 xl:px-0 xl:relative xl:top-0 xl:w-auto xl:h-auto xl:gap-x-6 xl:flex-row">
 				<Link className="mt-4 md:mt-8 lg:m-0 hover:text-blue-600 transition-colors xl:text-[13px]" href="/">Home</Link>
 				{MENULINKS.map((item) => (
 					<Link key={item.name} className="hover:text-blue-600 transition-colors xl:text-[13px]" href={item.link}>{item.name}</Link>
